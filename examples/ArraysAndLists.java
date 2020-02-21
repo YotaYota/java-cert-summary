b0772584702022320b6a233f3107c43d43d08104
jeppsson <johannes.jeppsson@bonniernews.se>
2020-02-21 12:37:51 +0100

    Update ArraysAndLists.java

--- examples/ArraysAndLists.java
+++ examples/ArraysAndLists.java
@@ -10,6 +10,7 @@ class ArraysAndLists {
   public static void compiles() {
     List<String> list = new ArrayList<>(List.of("A", "B"));
     String[] array1 = {"C"};
+    // return list as array with runtime type of array1
     String[] array2 = list.toArray(array1);
     array1[0] = "D";
     out.println(Arrays.toString(array2));
